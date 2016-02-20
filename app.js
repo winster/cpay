665d8304ad5d8e77fc4a820f773196b79737ca67
unknown <wtjose@gmail.com>
2016-02-20 18:54:33 +0530

    added secure server

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    https = require("https"),
+    fs = require("fs");
 
 var app = express();
 app.use(express.static(__dirname+'/public'));
@@ -7,8 +9,17 @@ app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
 require("./router.js")(app);
-app.listen(app.get('port'), function() {
+/*app.listen(app.get('port'), function() {
   console.log('Cafe app is running on port', app.get('port'));
+});*/
+var secureServer = https.createServer({
+    key: fs.readFileSync('./ssl/server.key'),
+    cert: fs.readFileSync('./ssl/server.crt'),
+    ca: fs.readFileSync('./ssl/cpay-ssl.crt'),
+    requestCert: true,
+    rejectUnauthorized: false
+}, app).listen('443', function() {
+    console.log("Secure Express server listening on port 8443");
 });
 exports = module.exports = app;
 
